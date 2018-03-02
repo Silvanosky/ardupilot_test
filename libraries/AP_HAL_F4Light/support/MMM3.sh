f6616e1c8f938ce0adefbf00fea325d242e1cc08
night-ghost <night_ghost@ykoctpa.ru>
2018-03-02 10:51:45 +0500

    fixed OSD compilation issue

---
+++ libraries/AP_HAL_F4Light/support/MMM3.sh
@@ -0,0 +1,73 @@
+#git submodule init && git submodule update
+export TOOLCHAIN
+
+ROOT=`cd ../../..; pwd`
+
+export PATH=/usr/local/bin:$PATH
+
+echo $ROOT
+
+mkdir -p $ROOT/Release/Copter
+mkdir -p $ROOT/Release/Plane
+
+( # Cl_Racing F4 board
+ cd $ROOT/ArduCopter
+ make f4light VERBOSE=1 BOARD=f4light_cl_racing  && (
+
+ cp $ROOT/ArduCopter/f4light_cl_racing.bin $ROOT/Release/Copter
+ cp $ROOT/ArduCopter/f4light_cl_racing.hex $ROOT/Release/Copter
+ cp $ROOT/ArduCopter/f4light_cl_racing.dfu $ROOT/Release/Copter
+
+ make f4light-clean
+
+ )
+) && (
+ cd $ROOT/ArduPlane
+ make f4light-clean
+ make f4light VERBOSE=1 BOARD=f4light_Airbot && (
+
+ cp $ROOT/ArduPlane/f4light_cl_racing.bin $ROOT/Release/Plane
+ cp $ROOT/ArduPlane/f4light_cl_racing.hex $ROOT/Release/Plane
+ cp $ROOT/ArduPlane/f4light_cl_racing.dfu $ROOT/Release/Plane
+
+ make f4light-clean
+
+ )
+) && ( # AirBotF4 board
+ cd $ROOT/ArduCopter
+# make f4light-clean
+ make f4light VERBOSE=1 BOARD=f4light_AirbotV2  && (
+
+ cp $ROOT/ArduCopter/f4light_AirbotV2.bin $ROOT/Release/Copter
+ cp $ROOT/ArduCopter/f4light_AirbotV2.hex $ROOT/Release/Copter
+ cp $ROOT/ArduCopter/f4light_AirbotV2.dfu $ROOT/Release/Copter
+
+
+ )
+) && (
+ cd $ROOT/ArduPlane
+ make f4light-clean
+ make f4light VERBOSE=1 BOARD=f4light_AirbotV2 && (
+
+ cp $ROOT/ArduPlane/f4light_AirbotV2.bin $ROOT/Release/Plane
+ cp $ROOT/ArduPlane/f4light_AirbotV2.hex $ROOT/Release/Plane
+ cp $ROOT/ArduPlane/f4light_AirbotV2.dfu $ROOT/Release/Plane
+ )
+
+) && (
+ cd $ROOT
+
+ zip -r latest.zip Release
+ git add . -A
+)
+
+
+
+
+
+
+
+
+
+
+
