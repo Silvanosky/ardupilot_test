2170d081ca10627cf0d2a7eb6ddbde8aee2a5b32
Charles Villard <charles.villard@epita.fr>
2019-03-12 10:16:25 +0100

    add: i2c: Add the i2c device driver

--- libraries/AP_HAL_ESP32/I2CDevice.h
+++ libraries/AP_HAL_ESP32/I2CDevice.h
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 /*
  * This file is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -14,7 +12,6 @@
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
->>>>>>> HAL_ESP32: added new board and i2c driver
 #pragma once
 
 #include <inttypes.h>
@@ -22,141 +19,6 @@
 #include <AP_HAL/HAL.h>
 #include <AP_HAL/I2CDevice.h>
 #include <AP_HAL/utility/OwnPtr.h>
-<<<<<<< HEAD
-#include "AP_HAL_ESP32.h"
-
-#if HAL_USE_I2C == TRUE
-
-#include "Semaphores.h"
-#include "DeviceBus.h"
-
-#include "driver/i2c.h"
-
-namespace ESP32
-{
-
-class I2CBus : public DeviceBus
-{
-public:
-        I2CBus();
-        ~I2CBus();
-
-        void init();
-
-        i2c_config_t cfg;
-        uint8_t bus;
-};
-
-class I2CDevice : public AP_HAL::I2CDevice
-{
-public:
-        static I2CDevice *from(AP_HAL::I2CDevice *dev)
-        {
-                return static_cast<I2CDevice *>(dev);
-        }
-
-
-        I2CDevice(uint8_t busnum, uint8_t address, uint32_t bus_clock,
-                  bool use_smbus,
-                  uint32_t timeout_ms);
-
-        ~I2CDevice();
-
-        /* See AP_HAL::I2CDevice::set_address() */
-        void set_address(uint8_t address) override
-        {
-                _address = address;
-        }
-
-        /* See AP_HAL::I2CDevice::set_retries() */
-        void set_retries(uint8_t retries) override
-        {
-                _retries = retries;
-        }
-
-        /* See AP_HAL::Device::set_speed(): Empty implementation, not supported. */
-        bool set_speed(enum Device::Speed speed) override
-        {
-                return true;
-        }
-
-        /* See AP_HAL::Device::transfer() */
-        bool transfer(const uint8_t *send, uint32_t send_len,
-                      uint8_t *recv, uint32_t recv_len) override;
-
-        bool read_registers_multiple(uint8_t first_reg, uint8_t *recv,
-                                     uint32_t recv_len, uint8_t times) override;
-
-        /* See AP_HAL::Device::register_periodic_callback() */
-        AP_HAL::Device::PeriodicHandle register_periodic_callback(
-                uint32_t period_usec, AP_HAL::Device::PeriodicCb) override;
-
-        /* See AP_HAL::Device::adjust_periodic_callback() */
-        bool adjust_periodic_callback(AP_HAL::Device::PeriodicHandle h,
-                                      uint32_t period_usec) override;
-
-        AP_HAL::Semaphore *get_semaphore() override
-        {
-                // if asking for invalid bus number use bus 0 semaphore
-                return &bus.semaphore;
-        }
-
-        void set_split_transfers(bool set) override
-        {
-                _split_transfers = set;
-        }
-
-private:
-        I2CBus &bus;
-        bool _transfer(const uint8_t *send, uint32_t send_len,
-                       uint8_t *recv, uint32_t recv_len);
-
-        /* I2C interface #2 */
-        uint8_t _retries;
-        uint8_t _address;
-
-        bool _split_transfers;
-        uint32_t _timeout_ms;
-        bool _use_smbus;
-};
-
-class I2CDeviceManager : public AP_HAL::I2CDeviceManager
-{
-public:
-        friend class I2CDevice;
-
-        // constructor
-        I2CDeviceManager();
-
-        AP_HAL::OwnPtr<AP_HAL::I2CDevice> get_device(uint8_t bus, uint8_t address,
-                        uint32_t bus_clock = 400000,
-                        bool use_smbus = false,
-                        uint32_t timeout_ms = 4) override;
-
-        /*
-          get mask of bus numbers for all configured I2C buses
-         */
-        uint32_t get_bus_mask(void) const override;
-
-        /*
-          get mask of bus numbers for all configured external I2C buses
-         */
-        uint32_t get_bus_mask_external(void) const override;
-
-        /*
-          get mask of bus numbers for all configured internal I2C buses
-         */
-        uint32_t get_bus_mask_internal(void) const override;
-
-
-private:
-        static I2CBus businfo[];
-
-};
-}
-
-#endif // HAL_USE_I2C
-=======
 
 #include "Semaphores.h"
 #include "Scheduler.h"
@@ -264,5 +126,3 @@ public:
     uint32_t get_bus_mask_internal(void) const override;
 };
 }
-
->>>>>>> HAL_ESP32: added new board and i2c driver
