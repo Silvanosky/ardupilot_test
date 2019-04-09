80333a03385eff6f5492fc7a1805f004aaaebdeb
Charles Villard <charles.villard@epita.fr>
2019-04-09 18:23:45 +0200

    ESP32: rcoutput: get changes from buzz

--- libraries/AP_HAL_ESP32/RCOutput.cpp
+++ libraries/AP_HAL_ESP32/RCOutput.cpp
@@ -1,142 +1,233 @@
+/*
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Code by David "Buzz" Bussenschutt and others
+ *
+ */
+
 #include "RCOutput.h"
+
+
+
+//HINTS: see more about MCPWM peripheral here: https://docs.espressif.com/projects/esp-idf/en/latest/api-reference/peripherals/mcpwm.html
+// and example code here: https://github.com/espressif/esp-idf/blob/e7f85f1987aa9479c2dbab638ca83bcaef99be00/examples/peripherals/mcpwm/mcpwm_basic_config/main/mcpwm_basic_config_example.c
+// and servo example here: https://github.com/espressif/esp-idf/blob/master/examples/peripherals/mcpwm/mcpwm_servo_control/main/mcpwm_servo_control_example.c
 
 #include <AP_HAL/AP_HAL.h>
 
+// this is the first/only "group" of 4 GPIO devices
 #define LIST_GROUP 0
 #define NUM_GROUPS ARRAY_SIZE(pwm_group_list)
+
 #define CHAN_DISABLED 255
 
 using namespace ESP32;
 
 struct RCOutput::pwm_group RCOutput::pwm_group_list[] = { LIST_GROUP };
 
+//You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000 mocrosecond
+#define SERVO_MIN_PULSEWIDTH 1000 //Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH 2000 //Maximum pulse width in microsecond
+#define SERVO_MAX_DEGREE 90 //Maximum angle in degree upto which servo can rotate
+/**
+ * @brief Use this function to calcute pulse width for per degree rotation
+ * @param  degree_of_rotation the angle in degree to which servo has to rotate
+ * @return
+ *     - calculated pulse width
+ */
+static uint32_t servo_per_degree_init(uint32_t degree_of_rotation)
+{
+    uint32_t cal_pulsewidth = 0;
+    cal_pulsewidth = (SERVO_MIN_PULSEWIDTH + (((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * (degree_of_rotation)) / (SERVO_MAX_DEGREE)));
+    return cal_pulsewidth;
+}
 
-void RCOutput::init() {
-    for (uint8_t i = 0; i < NUM_GROUPS; i++ ) {
-	pwm_group &group = pwm_group_list[i];
-	group.current_mode = MODE_PWM_NORMAL;
-	for (uint8_t j = 0; j < 4; j++ ) {
-	    pwm_out out = group.out_list[j];
-	    uint8_t chan = out.chan;
-	    if (chan >= 12) //chercher la valeur exacte
-		out.chan = CHAN_DISABLED;
-	    if (out.chan != CHAN_DISABLED) {
-		group.ch_mask |= (1U<<out.chan);
-		mcpwm_gpio_init(out.unit_num, out.io_signal, out.gpio_num);
-		mcpwm_config_t pwm_config;
-		pwm_config.frequency = 1000;    //frequency = 500Hz,
-		pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
-		pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
-		pwm_config.counter_mode = MCPWM_UP_COUNTER;
-		pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
-		mcpwm_init(out.unit_num, out.timer_num, &pwm_config);
-	    }
-	}
-    }
-    set_freq(0xFFFF ^ ((1U<<0)-1), 500);
+void RCOutput::init()
+{
+	   printf("RCOutput::init()\n");
+
+	   	// configure all 6 GPIOs the same at the same time... as OUTPUTS
+		#define GPIO_BIT_MASK  ((1ULL<<GPIO_NUM_21) | (1ULL<<GPIO_NUM_22) | (1ULL<<GPIO_NUM_25) | (1ULL<<GPIO_NUM_27) | (1ULL<<GPIO_NUM_32) | (1ULL<<GPIO_NUM_33))
+		gpio_config_t io_conf;
+		io_conf.intr_type = GPIO_INTR_DISABLE;
+		io_conf.mode = GPIO_MODE_OUTPUT;
+		io_conf.pin_bit_mask = GPIO_BIT_MASK;
+		io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+		io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+		gpio_config(&io_conf);
+
+
+	   	// list of 6 pins we'll be using for PWM out:
+		int myints[] = {33,32,21,22,25,27};
+		// 0A,0B,1A,1B,2A,2B timers, signals and operators.
+		mcpwm_timer_t mytimers[] = {MCPWM_TIMER_0,MCPWM_TIMER_0,MCPWM_TIMER_1,MCPWM_TIMER_1,MCPWM_TIMER_2,MCPWM_TIMER_2};
+		mcpwm_io_signals_t mysignals[] = {MCPWM0A,MCPWM0B,MCPWM1A,MCPWM1B,MCPWM2A,MCPWM2B};
+		mcpwm_operator_t myops[] = {MCPWM_OPR_A,MCPWM_OPR_B,MCPWM_OPR_A,MCPWM_OPR_B,MCPWM_OPR_A,MCPWM_OPR_B};
+		// setup all the timers and signals etc
+		for ( int i = 0 ; i < 6; i++) {
+			   pwm_out out;
+			   out.gpio_num = myints[i];
+			   out.unit_num = MCPWM_UNIT_0;
+			   out.timer_num = mytimers[i];
+			   out.io_signal = mysignals[i];
+			   out.op = myops[i];  // MCPWM_OPR_A = 0
+			   out.chan =  i ; // 0 = CH_1; // see AP_HAL/RCOutput.h, so 0-5 is CH1,CH2,CH3,CH4,CH5,CH6
+			   pwm_group_list[0].out_list[i] = out;
+		}
+
+        for (uint8_t i = 0; i < NUM_GROUPS; i++ ) {
+                pwm_group &group = pwm_group_list[i];
+                group.current_mode = MODE_PWM_NORMAL;
+                for (uint8_t j = 0; j < 5; j++ ) { // max 6 outputs per PWM UNIT,
+                        pwm_out out = group.out_list[j];
+                        uint8_t chan = out.chan;
+                        if (chan >= 12) //chercher la valeur exacte
+                                out.chan = CHAN_DISABLED;
+                        if (out.chan != CHAN_DISABLED) {
+                                group.ch_mask |= (1U << out.chan);
+                                mcpwm_gpio_init(out.unit_num, out.io_signal, out.gpio_num);
+                                mcpwm_config_t pwm_config;
+                                pwm_config.frequency = 50;    //frequency = 400Hz, which just happens to be 8 channels at 50Hz typical rate
+                                pwm_config.cmpr_a = 10;    //duty cycle of PWMxA = 0
+                                pwm_config.cmpr_b = 90;    //duty cycle of PWMxb = 0
+                                pwm_config.counter_mode = MCPWM_UP_COUNTER;
+                                pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+                                mcpwm_init(out.unit_num, out.timer_num, &pwm_config);
+                        }
+                }
+        }
+        //set_freq(0xFFFF ^ ((1U << 0) - 1), 500);
 #ifdef HAL_GPIO_PIN_SAFETY_IN
-    safety_state = AP_HAL::Util::SAFETY_DISARMED;
+        safety_state = AP_HAL::Util::SAFETY_DISARMED;
 #endif
 
 }
 
-void RCOutput::set_freq(uint32_t chmask, uint16_t freq_hz) {
-    for (uint8_t i = 0; i < NUM_GROUPS; i++) {
-	pwm_group &group = pwm_group_list[i];
-	if (freq_hz > 400 && group.current_mode != MODE_PWM_BRUSHED)
-	    freq_hz = 400;
-	if ((group.ch_mask & chmask) != 0) {
-	    for (int j = 0; j < 4; j++) {
-		pwm_out out = group.out_list[j];
-		mcpwm_set_frequency(out.unit_num, out.timer_num, freq_hz);
-	    }
-	}
-    }
+
+
+void RCOutput::set_freq(uint32_t chmask, uint16_t freq_hz)
+{
+        for (uint8_t i = 0; i < NUM_GROUPS; i++) {
+                pwm_group &group = pwm_group_list[i];
+                if (freq_hz > 400 && group.current_mode != MODE_PWM_BRUSHED)
+                        freq_hz = 400;
+                if ((group.ch_mask & chmask) != 0) {
+                        for (int j = 0; j < 4; j++) {
+                                //pwm_out out = group.out_list[j];
+                                //mcpwm_set_frequency(out.unit_num, out.timer_num, freq_hz);
+                        }
+                }
+        }
 }
 
 
-uint16_t RCOutput::get_freq(uint8_t chan) {
-    for (uint8_t i = 0; i < NUM_GROUPS; i++ ) {
-	pwm_group &group = pwm_group_list[i];
-	for (uint8_t j = 0; j < 4; j++) {
-	    pwm_out &out = group.out_list[j];
-	    if (out.chan == chan)
-		return mcpwm_get_frequency(out.unit_num, out.timer_num);
-	}
-    }
-    return 0;
+uint16_t RCOutput::get_freq(uint8_t chan)
+{
+        for (uint8_t i = 0; i < NUM_GROUPS; i++ ) {
+                pwm_group &group = pwm_group_list[i];
+                for (uint8_t j = 0; j < 4; j++) {
+                        pwm_out &out = group.out_list[j];
+                        if (out.chan == chan)
+                                return mcpwm_get_frequency(out.unit_num, out.timer_num);
+                }
+        }
+        return 0;
 }
 
-void RCOutput::enable_ch(uint8_t chan) {
-    for (uint8_t i = 0; i < NUM_GROUPS; i++ ) {
-	pwm_group &group = pwm_group_list[i];
-	for (uint8_t j = 0; j < 4; j++) {
-	    pwm_out &out = group.out_list[j];
-	    if (out.chan == chan)
-		mcpwm_start(out.unit_num, out.timer_num);
-	}
-    }
+void RCOutput::enable_ch(uint8_t chan)
+{
+        for (uint8_t i = 0; i < NUM_GROUPS; i++ ) {
+                pwm_group &group = pwm_group_list[i];
+                for (uint8_t j = 0; j < 4; j++) {
+                        pwm_out &out = group.out_list[j];
+                        if (out.chan == chan)
+                                mcpwm_start(out.unit_num, out.timer_num);
+                }
+        }
 }
 
-void RCOutput::disable_ch(uint8_t chan) {
-    for (uint8_t i = 0; i < NUM_GROUPS; i++ ) {
-   	pwm_group &group = pwm_group_list[i];
-        for (uint8_t j = 0; j < 4; j++) {
-    	    pwm_out &out = group.out_list[j];
-	    if (out.chan == chan)
-	    	mcpwm_stop(out.unit_num, out.timer_num);
-	}
-    }
+void RCOutput::disable_ch(uint8_t chan)
+{
+        for (uint8_t i = 0; i < NUM_GROUPS; i++ ) {
+                pwm_group &group = pwm_group_list[i];
+                for (uint8_t j = 0; j < 4; j++) {
+                        pwm_out &out = group.out_list[j];
+                        //if (out.chan == chan)
+                             //   mcpwm_stop(out.unit_num, out.timer_num);
+                }
+        }
 }
 
-void RCOutput::write(uint8_t chan, uint16_t period_us) {
-    if (chan >= max_channels)
-	return;
-    last_sent[chan] = period_us;
-    if (safety_state == AP_HAL::Util::SAFETY_DISARMED)
-	period_us = safe_pwm[chan];
-    period[chan] = period_us;
+void RCOutput::write(uint8_t chan, uint16_t period_us)
+{
+        if (chan >= max_channels)
+                return;
+        last_sent[chan] = period_us;
+        if (safety_state == AP_HAL::Util::SAFETY_DISARMED)
+                period_us = safe_pwm[chan];
+        period[chan] = period_us;
 
-    if (!corked)
-	push();
+        if (!corked)
+                push();
 //    mcpwm_set_duty_in_us(mcpwm.unit, mcpwm.timer, mcpwm.op, period_us);
 }
 
-void RCOutput::cork() {
-    corked = true;
+void RCOutput::cork()
+{
+        corked = true;
 }
 
-void RCOutput::push() {
-    corked = false;
-    for (uint8_t i = 0; i < NUM_GROUPS; i++) {
-	pwm_group &group = pwm_group_list[i];
-	for (uint8_t j = 0; j < 4; j++) {
-	    uint8_t chan = group.out_list[j].chan;
-	    if (chan == CHAN_DISABLED)
-		continue;
-	    uint32_t period_us = period[chan];
-	    if (safety_state == AP_HAL::Util::SAFETY_DISARMED)
-		period_us = safe_pwm[chan];
-	    if (group.current_mode == MODE_PWM_BRUSHED) {
-		if (period_us <= _esc_pwm_min)
-		    period_us = 0;
-		if (period_us >= _esc_pwm_max)
-		    period_us = _esc_pwm_max - 1;
-	    }
-	    else if (group.current_mode == MODE_PWM_ONESHOT125)
-	    	period_us = ((chan/1000000U) * period_us) / 8U;
-	    else if (group.current_mode < MODE_PWM_DSHOT150)
-       		period_us = (chan/1000000U) * period_us;
-	    pwm_out &out = group.out_list[j];
-	    mcpwm_set_duty_in_us(out.unit_num, out.timer_num, out.op, period_us);
-	}
-    }
+void RCOutput::push()
+{
+        corked = false;
+        for (uint8_t i = 0; i < NUM_GROUPS; i++) {
+                pwm_group &group = pwm_group_list[i];
+                for (uint8_t j = 0; j < 4; j++) {
+                        uint8_t chan = group.out_list[j].chan;
+                        if (chan == CHAN_DISABLED)
+                                continue;
+                        uint32_t period_us = period[chan];
+                        if (safety_state == AP_HAL::Util::SAFETY_DISARMED)
+                                period_us = safe_pwm[chan];
+
+                        if (group.current_mode == MODE_PWM_BRUSHED) {
+                                if (period_us <= _esc_pwm_min)
+                                        period_us = 0;
+                                if (period_us >= _esc_pwm_max)
+                                        period_us = _esc_pwm_max - 1;
+                        } else if (group.current_mode == MODE_PWM_ONESHOT125){
+                                period_us = ((chan / 1000000U) * period_us) / 8U;}
+
+                        //else if (group.current_mode < MODE_PWM_DSHOT150)
+                        //        period_us = (chan / 1000000U) * period_us; // sets period_us to zero, should not.
+
+                        pwm_out &out = group.out_list[j];
+
+                 //       cal_pulsewidth = (SERVO_MIN_PULSEWIDTH +
+                 //       		(((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * (degree_of_rotation)) / (SERVO_MAX_DEGREE)));
+
+
+                        mcpwm_set_duty_in_us(out.unit_num, out.timer_num, out.op, period_us);
+                }
+        }
 }
 
-uint16_t RCOutput::read(uint8_t chan) {
-    if (chan >= max_channels)
-	return 0;
-    return period[chan];
+uint16_t RCOutput::read(uint8_t chan)
+{
+        if (chan >= max_channels)
+                return 0;
+        return period[chan];
 }
 
 void RCOutput::read(uint16_t *period_us, uint8_t len)
@@ -155,25 +246,29 @@ void RCOutput::read(uint16_t *period_us, uint8_t len)
         memcpy(period_us, period, len * sizeof(uint16_t));
 }
 
-uint16_t RCOutput::read_last_sent(uint8_t chan) {
-    if (chan >= max_channels)
-	return 0;
-    return last_sent[chan];
+uint16_t RCOutput::read_last_sent(uint8_t chan)
+{
+        if (chan >= max_channels)
+                return 0;
+        return last_sent[chan];
 }
 
-void RCOutput::read_last_sent(uint16_t* period_us, uint8_t len) {
-    if (len > max_channels)
-	len = max_channels;
-    for (uint8_t i=0; i<len; i++) {
-	period_us[i] = read_last_sent(i);
-    }
+void RCOutput::read_last_sent(uint16_t *period_us, uint8_t len)
+{
+        if (len > max_channels)
+                len = max_channels;
+        for (uint8_t i = 0; i < len; i++) {
+                period_us[i] = read_last_sent(i);
+        }
 }
 
-void RCOutput::force_safety_off(void) {
-    safety_state = AP_HAL::Util::SAFETY_ARMED;
+void RCOutput::force_safety_off(void)
+{
+        safety_state = AP_HAL::Util::SAFETY_ARMED;
 }
 
-bool RCOutput::force_safety_on(void) {
-    safety_state = AP_HAL::Util::SAFETY_DISARMED;
-    return true;
+bool RCOutput::force_safety_on(void)
+{
+        safety_state = AP_HAL::Util::SAFETY_DISARMED;
+        return true;
 }
--- libraries/AP_HAL_ESP32/RCOutput.h
+++ libraries/AP_HAL_ESP32/RCOutput.h
@@ -1,50 +1,70 @@
+/*
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Code by David "Buzz" Bussenschutt and others
+ *
+ */
+
 #pragma once
+
 
 #include <AP_HAL/RCOutput.h>
 #include "HAL_ESP32_Namespace.h"
 #include "driver/mcpwm.h"
-
 #define HAL_PARAM_DEFAULTS_PATH nullptr
 #include <AP_HAL/Util.h>
 
-class ESP32::RCOutput : public AP_HAL::RCOutput
-{
+namespace ESP32 {
+
+class RCOutput : public AP_HAL::RCOutput {
 public:
-        void init() override;
+    void init() override;
 
-        void set_freq(uint32_t chmask, uint16_t freq_hz) override;
-        uint16_t get_freq(uint8_t chan) override;
-        void     enable_ch(uint8_t chan) override;
-        void     disable_ch(uint8_t chan) override;
+    void set_freq(uint32_t chmask, uint16_t freq_hz) override;
+    uint16_t get_freq(uint8_t chan) override;
 
-        void write(uint8_t chan, uint16_t period_us) override;
+    void     enable_ch(uint8_t chan) override;
+    void     disable_ch(uint8_t chan) override;
+
+    void write(uint8_t chan, uint16_t period_us) override;
 //    void set_reversible_mask(uint16_t chanmask) override;
-        void cork() override;
-        void push() override;
+    void cork() override;
+    void push() override;
 
-        uint16_t read(uint8_t chan) override;
-        void     read(uint16_t *period_us, uint8_t len) override;
+    uint16_t read(uint8_t chan) override;
+    void     read(uint16_t* period_us, uint8_t len) override;
 
-        uint16_t read_last_sent(uint8_t chan) override;
-        void     read_last_sent(uint16_t *period_us, uint8_t len) override;
+    uint16_t read_last_sent(uint8_t chan) override;
+    void     read_last_sent(uint16_t* period_us, uint8_t len) override;
 
 
 //    void set_safety_pwm(uint32_t chmask, uint16_t period_us) override;
 //    void set_failsafe_pwm(uint32_t chmask, uint16_t period_us) override;
-        bool force_safety_on(void) override;
-        void force_safety_off(void) override;
+    bool force_safety_on(void) override;
+    void force_safety_off(void) override;
 //    void force_safety_no_wait(void) override;
-        void set_esc_scaling(uint16_t min_pwm, uint16_t max_pwm) override
-        {
-                _esc_pwm_min = min_pwm;
-                _esc_pwm_max = max_pwm;
-        }
-        bool get_esc_scaling(uint16_t &min_pwm, uint16_t &max_pwm) override
-        {
-                min_pwm = _esc_pwm_min;
-                max_pwm = _esc_pwm_max;
-                return true;
-        }
+    void set_esc_scaling(uint16_t min_pwm, uint16_t max_pwm) override {
+       _esc_pwm_min = min_pwm;
+       _esc_pwm_max = max_pwm;
+}
+    bool get_esc_scaling(uint16_t &min_pwm, uint16_t &max_pwm) override {
+        min_pwm = _esc_pwm_min;
+        max_pwm = _esc_pwm_max;
+       return true;
+}
+
+
 //    float scale_esc_to_unity(uint16_t pwm) override;
 //    bool enable_px4io_sbus_out(uint16_t rate_hz) override;
 //    void timer_tick(void) override;
@@ -55,7 +75,6 @@ public:
 //    void set_output_mode(uint16_t mask, enum output_mode mode) override;
 //    void set_default_rate(uint16_t rate_hz) override;
 //    void set_telem_request_mask(uint16_t mask) override;
-
 
 private:
     struct pwm_out {
@@ -68,7 +87,7 @@ private:
 };
 
     struct pwm_group {
-	pwm_out out_list[4];
+	pwm_out out_list[6];
 	enum output_mode current_mode;
 	uint16_t ch_mask;
     };
@@ -83,5 +102,8 @@ private:
     uint16_t last_sent[max_channels];
     uint16_t safe_pwm[max_channels];
 
-    uint8_t chan_offset;
+	uint8_t chan_offset;
+
 };
+
+}
