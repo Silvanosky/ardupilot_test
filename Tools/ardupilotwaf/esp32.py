93d3447573118789e357793ebb53b0bb00fffe73
Charles Villard <charles.villard@epita.fr>
2019-04-11 18:46:47 +0200

    ESP32: sub: Add the ardusub project build system

--- Tools/ardupilotwaf/esp32.py
+++ Tools/ardupilotwaf/esp32.py
@@ -23,6 +23,7 @@ def configure(cfg):
     env.AP_HAL_PLANE = srcpath('libraries/AP_HAL_ESP32/plane')
     env.AP_HAL_COPTER = srcpath('libraries/AP_HAL_ESP32/copter')
     env.AP_HAL_ROVER = srcpath('libraries/AP_HAL_ESP32/rover')
+    env.AP_HAL_SUB = srcpath('libraries/AP_HAL_ESP32/sub')
     env.AP_PROGRAM_FEATURES += ['esp32_ap_program']
 
 def parse_inc_dir(lines):
@@ -54,6 +55,16 @@ class build_esp32_image_rover(Task.Task):
     '''build an esp32 image'''
     color='CYAN'
     run_str="cd ${AP_HAL_ROVER}&&'${MAKE}' V=1"
+    always_run = True
+    def keyword(self):
+        return "Generating"
+    def __str__(self):
+        return self.outputs[0].path_from(self.generator.bld.bldnode)
+
+class build_esp32_image_sub(Task.Task):
+    '''build an esp32 image'''
+    color='CYAN'
+    run_str="cd ${AP_HAL_SUB}&&'${MAKE}' V=1"
     always_run = True
     def keyword(self):
         return "Generating"
@@ -99,5 +110,16 @@ def esp32_firmware(self):
         cmd = "cd {0}&&{1} showinc".format(self.env.AP_HAL_ROVER, self.env.MAKE[0])
         result = subprocess.check_output(cmd, shell=True)
         self.env.INCLUDES += parse_inc_dir(result)
+    if str(self.link_task.outputs[0]).endswith('libardusub.a'):
+        #build final image
+        src_in = [self.bld.bldnode.find_or_declare('lib/libArduSub_libs.a'),
+                  self.bld.bldnode.find_or_declare('lib/bin/libardusub.a')]
+        img_out = self.bld.bldnode.find_or_declare('idf-sub/ardusub.elf')
+        generate_bin_task = self.create_task('build_esp32_image_sub', src=src_in, tgt=img_out)
+        generate_bin_task.set_run_after(self.link_task)
 
+        #add generated include files
+        cmd = "cd {0}&&{1} showinc".format(self.env.AP_HAL_SUB, self.env.MAKE[0])
+        result = subprocess.check_output(cmd, shell=True)
+        self.env.INCLUDES += parse_inc_dir(result)
 
