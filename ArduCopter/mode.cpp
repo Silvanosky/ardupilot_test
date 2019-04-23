ArduPilot/ardupilot

Copter: factorize arm or land check

ArduCopter/mode.cpp
@@ -390,6 +390,14 @@ bool Copter::Mode::_TakeOff::triggered(const float target_climb_rate) const
     return true;
 }
 
+bool Copter::Mode::check_disarmed_or_landed() const
+{
+    if (!motors->armed() || !ap.auto_armed || ap.land_complete) {
+        return true;
+    }
+    return false;
+}
+
 void Copter::Mode::zero_throttle_and_relax_ac(bool spool_up)
 {
     if (spool_up) {

ArduCopter/mode_zigzag.cpp
@@ -38,7 +38,7 @@ void Copter::ModeZigZag::run()
     pos_control->set_max_accel_z(g.pilot_accel_z);
 
     // if not auto armed or motors not enabled set throttle to zero and exit immediately
-    if (!motors->armed() || !ap.auto_armed || !motors->get_interlock() || ap.land_complete) {
+    if (check_disarmed_or_landed() || !motors->get_interlock() ) {
         zero_throttle_and_relax_ac(copter.is_tradheli() && motors->get_interlock());
         return;
     }

ArduCopter/takeoff.cpp
@@ -138,7 +138,7 @@ void Copter::Mode::auto_takeoff_set_start_alt(void)
     // start with our current altitude
     auto_takeoff_no_nav_alt_cm = inertial_nav.get_altitude();
     
-    if (!motors->armed() || !ap.auto_armed || !motors->get_interlock() || ap.land_complete) {
+    if (check_disarmed_or_landed() || !motors->get_interlock()) {
         // we are not flying, add the wp_navalt_min
         auto_takeoff_no_nav_alt_cm += g2.wp_navalt_min * 100;
     }

Silvanosky/ardupilot_test

ArduCopter/mode.cpp
@@ -390,6 +390,14 @@ bool Copter::Mode::_TakeOff::triggered(const float target_climb_rate) const
     return true;
 }
 
+bool Copter::Mode::is_disarmed_or_landed() const
+{
+    if (!motors->armed() || !ap.auto_armed || ap.land_complete) {
+        return true;
+    }
+    return false;
+}
+
 void Copter::Mode::zero_throttle_and_relax_ac(bool spool_up)
 {
     if (spool_up) {

ArduCopter/mode_zigzag.cpp
@@ -38,7 +38,7 @@ void Copter::ModeZigZag::run()
     pos_control->set_max_accel_z(g.pilot_accel_z);
 
     // if not auto armed or motors not enabled set throttle to zero and exit immediately
-    if (!motors->armed() || !ap.auto_armed || !motors->get_interlock() || ap.land_complete) {
+    if (is_disarmed_or_landed() || !motors->get_interlock() ) {
         zero_throttle_and_relax_ac(copter.is_tradheli() && motors->get_interlock());
         return;
     }

ArduCopter/takeoff.cpp
@@ -138,7 +138,7 @@ void Copter::Mode::auto_takeoff_set_start_alt(void)
     // start with our current altitude
     auto_takeoff_no_nav_alt_cm = inertial_nav.get_altitude();
     
-    if (!motors->armed() || !ap.auto_armed || !motors->get_interlock() || ap.land_complete) {
+    if (is_disarmed_or_landed() || !motors->get_interlock()) {
         // we are not flying, add the wp_navalt_min
         auto_takeoff_no_nav_alt_cm += g2.wp_navalt_min * 100;
     }